415e4db4673b6d3c58d680b1ca6d4354fe77fc1b
md2d-tlq <md2d_tlq@sina.com>
2019-12-05 09:45:47 +0800

    修改config.php中的上传文件的路径配置

--- php/config.php
+++ php/config.php
@@ -7,8 +7,8 @@
 		public static $db_username = 'root';
 		public static $db_password = '123456';
 		
-		public static $question_file_dir = 'question_file';
-		public static $answer_file_dir = 'answer_file';
+		public static $question_file_dir = './question_file';
+		public static $answer_file_dir = './answer_file';
 
 		public static $file_allow_ext = array('jpeg', 'jpg', 'txt', 'png', 'bmp', 'zip', 'rar', 'xlsx');
 		public static $file_max_size = 5242880;
